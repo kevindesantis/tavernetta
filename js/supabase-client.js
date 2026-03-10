e4aec12beeaea91343df75696d15d13beae772cb
kevindesantis <kevindavide31@gmail.com>
2026-03-10 23:46:17 +0100

    Add files via upload

---
+++ js/supabase-client.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://puygpylapxsxyuwggetq.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1eWdweWxhcHhzeHl1d2dnZXRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxNzUxNTAsImV4cCI6MjA4ODc1MTE1MH0.H8gURFF2n4p_luOj-_k6Br9LaA9tlqfEj8AINdhted0";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
